789e700edf2ed2cee255d16813e36e2329bd83a2
agbeyeye <dnewlife0@gmail.com>
2020-07-08 04:36:42 -1200

    module 1 assignment

---
+++ module1-Solution/app.js
@@ -0,0 +1,34 @@
+(function(){
+	'use strict';
+
+	angular.module("LunchCheck",[])
+	.controller("LunchCheckController", LunchCheckController);
+
+	LunchCheckController.$inject=['$scope'];
+
+	function LunchCheckController($scope){
+		$scope.lunchItems ='';
+
+		$scope.checkNumberOfItems = function(){
+			if($scope.lunchItems!=""){
+
+				var items = $scope.lunchItems.split(',');
+				items = items.filter(Boolean);
+				
+				if(items.length>0 && items.length<=3){
+					$scope.message = "Enjoy!";
+				}
+				else if(items.length>3){
+					$scope.message ="Too much!";
+				}
+			}else
+			{
+				$scope.message ="Please enter data first!";
+			}
+			
+
+		}
+
+		
+	}
+})();
