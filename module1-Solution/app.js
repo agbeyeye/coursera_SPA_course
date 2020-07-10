93a211de4589923aaf90bd2276dbcb25379cf87d
agbeyeye <dnewlife0@gmail.com>
2020-07-10 05:37:54 -1200

    Module 1 solution

--- module1-Solution/app.js
+++ module1-Solution/app.js
@@ -10,18 +10,30 @@
 		$scope.lunchItems ='';
 
 		$scope.checkNumberOfItems = function(){
+			//Input field must not be null
 			if($scope.lunchItems!=""){
-
+				//get list  of  items from string
 				var items = $scope.lunchItems.split(',');
+				//remove empty items in between comma
 				items = items.filter(Boolean);
+				//store number of items in scope variable
+				$scope.numberOfItems = items.length;
 				
-				if(items.length>0 && items.length<=3){
+				//if number of items is between 1-3
+				if( $scope.numberOfItems>0 && $scope.numberOfItems<=3){
 					$scope.message = "Enjoy!";
 				}
-				else if(items.length>3){
+				//if number of items is more than 3
+				else if($scope.numberOfItems>3){
 					$scope.message ="Too much!";
 				}
-			}else
+				//if number of items is 0
+				else if ($scope.numberOfItems==0){
+					$scope.message ="Please enter data first!";
+				}
+			}
+			//when the input field is empty
+			else
 			{
 				$scope.message ="Please enter data first!";
 			}
